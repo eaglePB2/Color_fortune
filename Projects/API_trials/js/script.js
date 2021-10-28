57fe6dbf0523677254a398810e9c4dcbdcc30194
EaglePB2 <eagleforceleader@hotmail.com>
2021-10-28 13:39:24 +0800

    Add files via upload

---
+++ Projects/API_trials/js/script.js
@@ -0,0 +1,28 @@
+let url = "quotes.json";
+fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (data) {
+        let nextthought = document.getElementById("nextthought");
+        nextthought.addEventListener("click", function () {
+            let countnum = document.getElementById("countnum");
+            let a = Math.floor(Math.random() * (1000 + 1));
+            countnum.value = a;
+            displaythought(countnum.value, data);
+        });
+    });
+
+function displaythought(index, data) {
+    let thought = document.getElementById("thought");
+
+    if (data[index].author == null) {
+        data[index].author = "unknown";
+    }
+
+    let htmlthought = `<div class="alert alert-outline-primary">
+			${data[index].text}<br>
+		</div>`;
+    thought.innerHTML = htmlthought;
+}
