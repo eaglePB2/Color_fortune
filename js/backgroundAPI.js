eee9a172788ce6b259d7d5d018ab764982fda22e
EaglePB2 <eagleforceleader@hotmail.com>
2022-06-15 11:53:13 +0800

    Change API

--- js/backgroundAPI.js
+++ js/backgroundAPI.js
@@ -1,5 +1,5 @@
 function change() {
-    document.getElementById('bgimg').style.backgroundImage = 'url(https://pic.re/images)';
+    document.getElementById('bgimg').style.backgroundImage = 'url(https://iw233.cn/api.php?sort=mp)';
 
     setInterval(function () {
         document.getElementById('bgimg').src =  document.getElementById('bgimg').src
