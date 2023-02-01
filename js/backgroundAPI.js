d4e5aaa4bca6634593068dbfa36d4e6b0e527a32
EaglePB2 <eagleforceleader@hotmail.com>
2023-02-01 15:36:48 +0800

    idk if it works

--- js/backgroundAPI.js
+++ js/backgroundAPI.js
@@ -2,7 +2,7 @@ function change() {
     document.getElementById('bgimg').style.backgroundImage = 'url(https://pic.re/image)';
 
     setInterval(function () {
-        document.getElementById('bgimg').src =  document.getElementById('bgimg').src
+        document.getElementById('bgimg').style.backgroundImage = 'url(https://pic.re/image)'
     }, 10000);
 }
 change();
