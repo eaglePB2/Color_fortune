d3bea6b6558e2cac3ce72fe552c749f57f903432
EaglePB2 <eagleforceleader@hotmail.com>
2023-08-02 15:38:05 +0800

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -1,21 +1,36 @@
-$(document).ready(function () {
-    const settings = {
-        "cache": false,
-        "dataType": "json",
-        "async": true,
-        "crossDomain": true,
-        "url": "../quotes.json",
-        "method": "GET"
+// Function to read the CSV file and get the quotes
+async function getQuotes() {
+    try {
+      const response = await fetch('quotes/csv/quotes.csv');
+      const data = await response.text();
+      const quotes = data.split('\n').filter(quote => quote.trim() !== '');
+      return quotes;
+    } catch (error) {
+      console.error('Error reading CSV file:', error);
+      return [];
     }
-    $.ajax(settings).done(function (response) {
-        var filterList = JSON.parse(JSON.stringify(response));
-        //这是一开始要写的
-        let a = Math.floor(Math.random() * (filterList.length));
-        document.getElementById('thought').innerHTML = filterList[a].text;
-        //每10秒要改的装置
-        setInterval(function(){
-        let a = Math.floor(Math.random() * (filterList.length));
-        document.getElementById('thought').innerHTML = filterList[a].text;
-    },10000);
-    })
-})
+}
+
+// Function to generate a random quote
+function generateRandomQuote(quotes) {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+}
+
+// Function to display the quote
+function displayQuote() {
+    getQuotes().then(quotes => {
+        if (quotes.length > 0) {
+            const randomQuote = generateRandomQuote(quotes);
+            $('#thought').text(randomQuote);
+        } else {
+            $('#thought').text('當你看到這句話的時候，他大概率是壞掉了……');
+        }
+    });
+}
+
+// Load the quote when the webpage is loaded
+$(document).ready(function () {
+    displayQuote();
+    setInterval(displayQuote, 10000); // Change quote every 10 seconds
+});
