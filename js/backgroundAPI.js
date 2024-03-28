7560649f1dfd0dd4c5eef4d5b7db65b52480ee15
EaglePB2 <eagleforceleader@hotmail.com>
2024-03-28 17:51:44 +0800

    Powered by vite

--- js/backgroundAPI.js
+++
@@ -1,8 +0,0 @@
-function change() {
-    document.getElementById('bgimg').style.backgroundImage = 'url(https://pic.re/image)';
-
-    setInterval(function () {
-        document.getElementById('bgimg').style.backgroundImage = 'url(https://pic.re/image)'
-    }, 10000);
-}
-change();
